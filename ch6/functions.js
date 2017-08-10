a6717816f6de2c67949c569b71dd94a32099a682
michaelvincerra <michael.vincerra@gmail.com>
2017-08-09 20:19:29 -0700

    Adds function.js in ch6.

--- ch6/functions.js
+++ ch6/functions.js
@@ -4,37 +4,120 @@
 
 'use strict';
 
-function getGreeting() {
-    return "Buongiorn a Tutti!"
+// function getGreeting() {
+//     return "Buongiorno a Tutti!"
+// }
+//
+// getGreeting();
+
+//
+//
+// function f(x) {
+//     console.log(`inside f: x=${x}`);
+//     x = 5;
+//     console.log(`inside f: x=${x} (after assignment)`);
+// }
+//
+// let x = 3;
+// console.log(`before calling f: x =${x}`);
+// f(x);     // Returns the value that f(x) resolves to, above.
+// console.log(`after calling f: x =${x}`);
+
+//////////////// KEEP ABOVE AND BELOW SEPARATE: COMMENT OUT EITHER/OR TO TEST.
+//
+
+// function f(o) {
+//     o.message = `set in f(previous value: '${o.message}')`;
+// }
+//
+// let o = {
+//         message: "initial value"
+// };
+//     console.log(`before calling f: o.message= "${o.message}"`);
+// f(o);
+// console.log(`after calling f: o.message="${o.message}"`);
+
+
+///////////////////
+
+
+// function f(o) {
+//     o.message = "set in f";
+//     o = {
+//         message: "new object!"
+//     };
+//     console.log(`inside f: o.message = "${o.message}" (after assignment)`);
+// }
+//
+// let o = {
+//    message: "initial value"
+// };
+//
+// console.log(`before calling f: o.message="${o.message}"`);
+// f(o);
+// console.log(`after calling f: o.message="${o.message}"`);
+
+
+// let o  = {message: 'initial value'};
+//
+// console.log(`before calling f: o.message="${o.message}"`);
+// f(o);
+//
+// console.log(`after calling f: o.message="${o.message}"`);
+
+
+//
+//    console.log(`inside f: o.message = "${o.message}" (after assignment)`);
+// }
+// let o  = {message: 'initial value'};
+//
+// console.log(`before calling f: o.message="${o.message}"`);
+// f(o);
+//
+// console.log(`after calling f: o.message="${o.message}"`);
+
+
+
+//////// DESTRUCTURING ARGUMENTS
+
+function getSentence({subject, verb, object}) {
+    return `${subject} ${verb} ${object}`;
 }
 
-getGreeting();
+const o = {
+    subject: "I",
+    verb: "love",
+    object: 'JavaScript!'
+};
+
+const arr = ["I", "love", "JavaScript as an array!"];
+
+getSentence(o);
 
 
+// function getSentence({subject, verb, object}) {
+//     return `${subject} ${verb} ${object}`;
+// }
+//
+// const o = {
+//     subject: "I",
+//     verb: "love",
+//     object: 'JavaScript!'
+// };
+//
+// const arr = ["I", "love", "JavaScript as an array!"];
+// getSentence(arr);
 
-function f(x) {
-    console.log(`inside f: x=${x}`);
-    x=5;
-    console.log(`inside f: x=${x} (after assignment)`);
+
+function addPrefix(prefix, ...words) {
+    const prefixedWords = [];  // list building pattern?
+    for(let i=0; i<words.length; i++){
+        prefixedWords[i] = prefix + words[i];
+    }
+    return prefixedWords;
 }
-
-let x = 3;
-console.log(`before calling f: x =${x}`);
-f(x);     // Returns the value that f(x) resolves to, above.
-console.log(`after calling f: x =${x}`);
+addPrefix("con", "verse", "vex");
 
 
-function f(o) {
-    o = {
-        message: "new object!"
-    };
-    console.log(`inside f: o.message = "${o.message}" (after assignment)`);
-}
-let o  = {message: 'initial value'};
-
-console.log(`before calling f: o.message="${o.message}"`);
-f(o);
-
-console.log(`after calling f: o.message="${o.message}"`);
 
 
