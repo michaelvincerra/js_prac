b8a990e726e064a9b7db4687c4d1e6d269ac2410
michaelvincerra <michael.vincerra@gmail.com>
2017-08-13 18:05:35 -0700

    Completes ch7, and scope.js

--- ch7/scope.js
+++ ch7/scope.js
@@ -93,18 +93,81 @@
 // console.log(typeof x);  //"x" is undefined; out of scope.
 
 // NESTED FUNCTION -- WITH TYPEOF OUT OF SCOPE
-{
-    // outer block
-    let x = "blue";
-    console.log(x);
+// {
+//     // OUTER block
+//     let x = "blue";
+//     console.log(x);
+//
+//     console.log(x);  //"x" is undefined; out of scope.
+//     {
+//         // INNER block
+//         const x = 3;        // VARIABLE MASKING
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+// console.log(typeof x);  //"x" is undefined; out of scope.
 
-    console.log(x);  //"x" is undefined; out of scope.
-    {
-        // block 2
-        const x = 3;        // VARIABLE MASKING
-        console.log(x);
-    }
+// CLOSURE
+//
+// let globalFunc;                 // undefined global function
+// {
+//     let blockVar = 'a';         // block-scoped variable
+//     globalFunc = function() {
+//         console.log(blockVar);
+//     }
+// }
+// globalFunc();
+
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+// (function(){
+//     // This is the IIFE body.
+// })();
+//
+// const message = (function(){
+//     const secret = "I'm a secret!";
+//     return `The secret is ${secret.length} characters long.`;
+// })();
+// console.log(message);
+
+// const f = (function(){
+//     let count = 0;
+//     return function() {
+//         return `I have been called ${++count} times.`;
+//     }
+// })();           /// Note: the '()' called at end invokes the anonymous function
+// console.log(f());
+// console.log(f());
+// console.log(f());
+
+// FUNCTION SCOPE AND HOISTING
+//
+// let var1;                   //WHEREAS LET CANNOT BE DECLARED BEFORE INVOCATION....
+// let var2 = undefined;
+// console.log(var1);
+// console.log(var2);
+// console.log(undefinedVar);
+//
+//
+// console.log((x);
+// console.log(var x = 3);         // VAR'S VALUE CAN BE DECLARED BEFORE INVOCATION.
+// console.log(x);
+
+
+var x;
+x;
+x = 3;
+x;
+
+console.log(x);
+
+
+var x = 3;      // Variables declared with var use a mechanism called 'hoisting',
+                // meaning any variables declared as such are hoisted to the top.
+                // Only the declaration, not the assignment, is hoisted. See var x = 2
+if(x === 3) {
+    var x = 2;
     console.log(x);
 }
-console.log(typeof x);  //"x" is undefined; out of scope.
-
+console.log(x);
