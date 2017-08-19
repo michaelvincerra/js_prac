47d70b5b76aa1f6d13f91f49e9a19502eec51911
michaelvincerra <michael.vincerra@gmail.com>
2017-08-18 21:26:14 -0700

    Add more array ex to ch8.

--- ch8/arrays.js
+++ ch8/arrays.js
@@ -12,7 +12,8 @@ arr1.slice(1, -2);
 arr1.slice(-2,-1);
 
 
-/// SPLICE
+// SPLICE
+// First arg, start index; second arg 0 if no deletion; and following are which numbers to add.
 const arr2 = [1,5,7];
 arr2.splice(1, 0, 2, 3, 4);
 arr2.splice(5,0,6);
@@ -21,7 +22,9 @@ arr2.splice(2, 1, 'a', 'b');
 
 
 
-// CUTTING AND REPLACING WITHIN AN ARRAY
-
-const arr3 = [1,2,3,4;
+// CUTTING AND REPLACING WITHIN AN ARRAY, new in ES6
+// First arg is where to copy to; second arg is wher to start copying from; and final (optional) is where to stop copying from.
+const arr3 = [1,2,3,4];
 arr3.copyWithin(1,2);
+arr3.copyWithin(2,0,2);
+arr3.copyWithin(0, -3, -1);
