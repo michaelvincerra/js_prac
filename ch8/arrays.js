367266cc51f080eebd5306279916e8c5fa2a3ade
michaelvincerra <michael.vincerra@gmail.com>
2017-08-19 18:18:06 -0700

    Adds array searching to ch8.

--- ch8/arrays.js
+++ ch8/arrays.js
@@ -52,3 +52,37 @@ const arr6 = [5,3,2,4,1];
 // sorts an array in place
 arr6.sort();
 
+
+const arr7 = [{name: "Silvia"}, {name: "Giacomo"}, {name: "Laura"}, {name: "Desdemona"}];
+
+// allows  to sort alphabetically by name 'property.'
+// console.log(arr7.sort((a,b) => a.name > b.name));
+
+// Sorted reverse alphabetically by second letter of name property [index number]
+console.log(arr7.sort((a,b) => a.name[1] < b.name[1]));
+
+
+// ARRAY SEARCHING
+// indexOf:  Returns index of the first element that it finds that's strictly equal to search
+// lastIndexOf: Returns the last index that matches search using reverse direction
+// Returns -1 if there is no match.
+
+const o = {name: "Giovanni"};
+
+const arr8 = [1, 5, "a", o, true, 5, [1,2], "9"];
+
+console.log(arr8.indexOf(5));
+console.log(arr8.lastIndexOf(5));
+console.log(arr8.indexOf("a"));
+console.log(arr8.lastIndexOf("a"));
+console.log(arr8.indexOf({name: "Giovanni"}));
+console.log(arr8.indexOf(o));
+console.log(arr8.indexOf([1,2]));
+console.log(arr8.indexOf("9"));
+console.log(arr8.indexOf(9));
+
+console.log(arr8.indexOf("a", 5));
+console.log(arr8.indexOf(5,5));
+console.log(arr8.lastIndexOf(5, 4));
+console.log(arr8.lastIndexOf(true, 3));
+
