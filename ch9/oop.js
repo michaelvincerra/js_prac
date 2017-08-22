9cc878e7343567ce2a3b96dd6e7f58aec3af5997
michaelvincerra <michael.vincerra@gmail.com>
2017-08-21 20:26:24 -0700

    Adds inheritance to ch9.

--- ch9/oop.js
+++ ch9/oop.js
@@ -109,36 +109,106 @@
 
 // STATIC METHODS
 
-class Car {
-    static getNextVin(){
-        return Car.nextVin++;
+// class Car {
+//     static getNextVin(){
+//         return Car.nextVin++;
+//     }
+//     constructor(make, model) {
+//         this.make = make;
+//         this.model = model;
+//         this.vin = Car.getNextVin();
+//     }
+//     static areSimilar(car1, car2) {
+//         return car1.make===car2.make && car1.model===car2.model;  // equality test is restrictive
+//     }
+//     static areSame(car1, car2){
+//         return car1.vin===car2.vin;
+//     }
+// }
+//
+// Car.nextVin = 0;
+//
+// const car1 = new Car("Tesla", "S");
+// const car2 = new Car("Mazda",  "3");
+// const car3 = new Car("Mazda",  "3");
+//
+// console.log(car1.vin);
+// console.log(car2.vin);
+// console.log(car3.vin);
+//
+// console.log(Car.areSimilar(car1, car2));
+// console.log(Car.areSimilar(car2, car3));
+// console.log(Car.areSame(car2, car3));
+// console.log(Car.areSame(car2, car2));
+
+// INHERITANCE
+
+class Vehicle {
+    constructor(){
+       this.passengers = [];
+       consol.log("Vehicle created");
     }
-    constructor(make, model) {
-        this.make = make;
-        this.model = model;
-        this.vin = Car.getNextVin();
-    }
-    static areSimilar(car1, car2) {
-        return car1.make===car2.make && car1.model===car2.model;  // equality test is restrictive
-    }
-    static areSame(car1, car2){
-        return car1.vin===car2.vin;
+    addPassenger(p) {
+        this.passengers.push(p);
     }
 }
 
-Car.nextVin = 0;
+class Car extends Vehicle {
+    constructor() {
+        super();
+        console.log("Car created");
+    }
+    deployAirbags(){
+        console.log("BWOOSH!");
+    }
+}
 
-const car1 = new Car("Tesla", "S");
-const car2 = new Car("Mazda",  "3");
-const car3 = new Car("Mazda",  "3");
+const v = new Vehicle();
+v.addPassenger("Frank");
+v.addPassenger("Judy");
+v.passengers;
 
-console.log(car1.vin);
-console.log(car2.vin);
-console.log(car3.vin);
+const c = new Car();
+c.addPassenger("Alice");
+c.addPassenger("Cameron");
+c.passengers;
+v.deployAirbags();
+c.deployAirbags();
 
-console.log(Car.areSimilar(car1, car2));
-console.log(Car.areSimilar(car2, car3));
-console.log(Car.areSame(car2, car3));
-console.log(Car.areSame(car2, car2));
+// Instnaces of the Car class can access all methods of the Vehicle class, but not the other way around.
 
-// INHERITANCE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
