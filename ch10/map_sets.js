8360cf647f318be418dd7283f7f3648909173720
michaelvincerra <michael.vincerra@gmail.com>
2017-08-22 18:16:59 -0700

    Adds Maps with .get, .has, keys(), values(), etc. to ch 10.

--- ch10/map_sets.js
+++ ch10/map_sets.js
@@ -13,7 +13,7 @@ const u2 = {name: "Jackson"};
 const u3 = {name: "Lolita"};
 const u4 = {name: "Arturo"};
 
-const userRoles = new Map();
+// const userRoles = new Map();
 
 // Use 'set'  to assign user to roles
 
@@ -35,3 +35,32 @@ const userRoles = new Map([
     [u2, "User"],
     [u3, "Admin"],
 ]);
+
+// in JS console, you can search object data:
+// What role does u1 have?: userRoles.get(u1)
+// Use 'has' to determine if a map contains a given key
+
+userRoles.has(u1);
+// returns true or false; this can be good in context of if/else statements
+
+// 'set' used in this context will replace previous value
+console.log(userRoles.get(u1));
+console.log(userRoles.set(u1, 'User'));  // Change from User to Admin
+console.log(userRoles.get(u1));
+
+// Detemine how many users exist: .size
+console.log(userRoles.size);
+
+// Use keys() to return keys in a map
+// Use values() return the values
+// Use entries() to return entries as arrays, where first element is key and second element is value
+
+for(let u of userRoles.keys())
+    console.log(u.name);
+
+for(let r of userRoles.values())
+    console.log(r);
+
+for(let ur of userRoles.entries())
+    console.log(`${ur[0].name}: ${ur[1]}`);
+
