79a66a34e3540f5b25a96c0d4460e38f312075d4
michaelvincerra <michael.vincerra@gmail.com>
2017-08-22 18:55:25 -0700

    Completes WeakMaps() in ch10.

--- ch10/map_sets.js
+++ ch10/map_sets.js
@@ -82,3 +82,24 @@ console.log(userRoles.size);
 
 userRoles.clear();
 console.log(userRoles.size);
+
+const SecretHolder = (function() {
+      const secrets = new WeakMap();
+      return class {
+          setSecret(secret) {
+              secrets.set(this, secret);
+          }
+          getSecret() {
+              return secrets.get(this);
+          }
+      }
+})();
+
+const a = new SecretHolder();
+const b = new SecretHolder();
+
+a.setSecret('secret A');
+b.setSecret('secret B');
+
+console.log(a.getSecret());
+console.log(b.getSecret());
