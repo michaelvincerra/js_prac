a710330476ab334a8f039768479871b7ffdb9865
michaelvincerra <michael.vincerra@gmail.com>
2017-08-29 22:46:01 -0700

    Adds Promise Chaining to ch14.

--- ch14/async_prog.js
+++ ch14/async_prog.js
@@ -194,16 +194,44 @@ class Countdown extends EventEmitter{
     }
 }
 
-const c = new Countdown(15, true)
-.on('tick', function(i){
-if(i>0) console.log(i + '...');
+//const c = new Countdown(15, true)
+//.on('tick', function(i){
+//if(i>0) console.log(i + '...');
+//});
+//
+//c.go()
+//
+//.then(function(){
+//console.log('GO!');
+//})
+//.catch(function(err){
+//console.error(err.message);
+//})
+
+
+//Promise Chaining
+
+function launch() {
+return new Promise(function(resolve, reject){
+console.log("Lift off!");
+setTimeout(function() {
+resolve("In orbit!");
+}, 2*1000);  // very fast rocket
 });
+}
+
+// Now chain this function to a countdown
+const c = new Countdown(5)
+    .on('tick', i => console.log(i+ '...'));
+
 
 c.go()
-
-.then(function(){
-console.log('GO!');
+    .then(launch)
+    .then(function(msg) {
+    console.log(msg);
 })
 .catch(function(err){
-console.error(err.message);
+console.error("Houston we have a problem.");
 })
+
+console.log(launch()); 
