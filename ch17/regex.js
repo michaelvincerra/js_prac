577409cfb8f17484abc26c806f9453cf9fefdbce
michaelvincerra <michael.vincerra@gmail.com>
2017-09-01 22:13:43 -0700

    Completes ch17 regex. Starts ch18, Javascript in the Browser.

--- ch17/regex.js
+++ ch17/regex.js
@@ -110,6 +110,160 @@ console.log(neatPhone);
 
 const field = '        something ';
 const valid = /\S/.test(field);
-console.log(field); 
+console.log(field);
+
+// REPETITION -- THERE'S JOY IN REPETITION...
+
+const matcher = beer99.match(/[0-9]+/);
+console.log(matcher);
+
+// Repetition modifier
+// {n}      /d{5}       = matches only 5-digit numbers
+// {n,}     /\d{5,}     = matches only 5-digit numbers OR longer ,
+// {n, m}   /\d{2,5]/   = matches only numbers that are at least 2 digits but NO MORE than 5 digits
+//  ?       /[a-z]\d?/i   =  matches letter followed by an optional digit
+
+// period matching
+const input1 = "Address: 333 Main St., Anywhere, NY, 55532. Phone: 555-555-5555.";
+const match2 = input1.match(/\d{5}.*/);
+
+const equation = "(2 + 3.5) *7";
+const matchme = equation.match(/\(\d \+ \d\.\d\) \* \d/);  // period matches any char EXCEPT new lines.
+console.log(matchme);
+
+// [\s\S]
+
+// GROUPING ALLOWS TO CONSTRUCT SUB-EXPRESSIONS
+// Groups are specified by parentheses: text.match(/..../i);
+
+const texty = "Visit oreilly.com today!";
+const match1 =texty.match(/[a-z]+(?:\.com|\.org|\.edu)/i);
+console.log(match1);
+
+const html1 = '<link rel="stylesheet" href="http://insecure.com/stuff.css">\n>' +
+'<link rel="stylesheet" href="https://secure.com/securestuff/css">\n' +
+'<link rel="stylesheet" href="//anything.com/flexible.css">';
+
+const matches1 = html1.match(/(?:https?)?\/\/[a-z][a-z[a-z0-9]+[a-z0-9]+/ig);
+console.log(matches1);
+
+// Lazy Matches vs. Greedy Matches
+
+const input3 = "Regex pros know the difference between\n" +
+"<i>greedy</i> and <i>lazy</i>matching.";
+const zappa = input3.replace(/<i>(.*)<\/i>/ig,  '<strong>$1</strong>');
+console.log(zappa);
+
+// REvise above line to make it lazy:
+const zappa1 = input3.replace(/<i>(.*?)<\/i>/ig,  '<strong>$1</strong>');        // NOTE: (.*?)...
+
+// BACK REFERENCES skipped --rarely used
+
+// REPLACING GROUPS
+//let html5 = '<a class="yep" href ="/yep" id="nope">Yep</a>;
+////html5 = html5.replace(/<a .*?(class=".*?").*?(href=".*?").*?>/, '<a $2 $1>');
+//console.log(html5);
+
+//FUNCTION REPLACEMENTS
+
+
+//function sanitizeATag(aTag) {
+//// get the parts of the tag...
+//const parts = aTag.match(/<a\s+(.*?)>(.*?)<\/a>/i); // parts[1] are the attributes of the opening <a> tag // parts[2] are what's between the <a> and </a> tags const attributes = parts[1]
+//          // then we split into individual attributes
+//.split(/\s+/);
+//return '<a ' + attributes
+//          // we only want class, id, and href attributes
+//.filter(attr => /^(?:class|id|href)[\s=]/i.test(attr)) // joined by spaces
+//.join(' ')
+// Function Replacements
+//| 251
+//// close the opening <a> tag
+//+ '>'
+//// add the contents
+//+ parts[2]
+//// and the closing tag + '</a>';
+//}
+
+
+// ANCHORING    ^.....$
+
+const string = " It was the best of  times, it was the worst of times.";
+const beginning = string.match(/^\w+/g);
+const end = string.match(/\w+$/g);
+const everything = string.match(/^.*$/g);
+const nomatch1 = string.match(/^best/ig);
+const nomatch2 = string.match(/^worst$/ig);
+
+console.log(beginning);
+console.log(end);
+console.log(everything);
+console.log(nomatch1);
+console.log(nomatch2);
+
+
+// WORD BOUNDARY MATCHING   \b
+
+const inputs = [ "john@doe.com",
+        "john@doe.com is my email",
+        "my email is john@doe.com",
+        "use john@doe.com, my email",
+        "my email:john@doe.com.",
+];
+
+const emailMatcher =
+/\b[a-z][a-z0-9._-]*@[a-z][a-z0-9_-]+\.[a-z]+(?:\.[a-z]+)?\b/ig;
+inputs.map( s=> s.replace(emailMatcher, '<a href"mailto:$&>$&</a>'));
+
+console.log(inputs);
+
+//  LOOKAHEADS: INDEPENDENT REGEX THAT DOESN'T CONSUME ANY INPUT
+
+//function validPassword(p){
+//return  /[A-Z]/.test(p) &&
+//        /[0-9]/.test(p) &&
+//        /[a-z]/.test(p) &&
+//        !/[^a-zA-Z0-9].test(p;)}
+//}
+//
+//console.log(validPassword);
+//
+//
+
+//REVISED
+
+function validPassword(p) {
+return /(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?!.*[^a-zA-Z0-9])/.test(p);
+}
+
+let samba = "12345Banter";
+
+function valid1(){
+if(validPassword(samba)) return valueOf(samba);
+}
+
+
+console.log(valid1);
+
+
+// CONSTRUCTING REGEXES DYNAMICALLY
+
+//const users = ["mary", "nick", "arthur", "sam", "yvette"]; const text = "User @arthur started the backup and 15:15, " +
+//"and @nick and @yvette restored it at 18:35.";
+//const userRegex = new RegExp(`@(?:${users.join('|')})\\b`, 'g'); text.match(userRegex); // [ "@arthur", "@nick", "@yvette" ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
