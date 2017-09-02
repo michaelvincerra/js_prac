829164641baff32f847810e202f1930692834cda
michaelvincerra <michael.vincerra@gmail.com>
2017-09-01 20:55:22 -0700

    Completes alternation, char sets and named char sets in ch17.

--- ch17/regex.js
+++ ch17/regex.js
@@ -62,4 +62,54 @@ console.log(output2);
 // SOPHISTICATED USE OF REGEX:  A PATTERN FOR CONSUMING INPUT STRINGS
 
 
-// REGEX ALTERNATIONS
+// REGEX ALTERNATIONS --next!
+// Find all the HTML tags that reference an external source
+// | - vertical bar means alternation
+
+//const html = 'HTML with <a href="/one">one link</a>, and some Javascript.' +
+//'<script src="regex.js"></script>';                             // Note: HTML tags appear inside '' quotations
+//const matches = html.match(/area|a|link|script|source/ig);      // first attempt;  /ig means ignore (i) case, and search globally (g).
+// revised:
+// const matches = /<area/<a/<link/<script/<source/
+
+// Other examples:
+ const html = '<br> [!CDATA[[<br>]]';
+ const matches = html.match(/<br>/ig);
+ console.log(matches);
+
+
+// REGEX Character Sets (SAME AS PYTHON)
+// Find all the numbers in a string
+
+const beer99 = "99 bottles of beer on the wall " ;
+//"take one down and pass it around -- " ;
+//...
+const m2 = beer99.match(/[0-9]/g);
+
+//const match = beer99.match(/[\-0-9a-z.]/ig);
+//console.log(match);
+
+// NEGATE character  sets with '^'
+
+const match = beer99.match(/[^\-0-9a-z.]/ig);
+// in above case this matches ONLY the whitespace; all else is negated by ^
+
+// Example of /s  -- whitespace
+
+const stuff =   'height:    9\n' +
+                'medium:    5\n' +
+                'low:       2\n' ;
+const levels = stuff.match(/:\s*[0-9]/g);
+
+// * -- means zero or more whitespaces
+
+const messyPhone = '(503) 555-5151';
+const neatPhone = messyPhone.replace(/\D/g, '') // \D --any char that's NOT 0-9
+console.log(neatPhone);
+
+
+const field = '        something ';
+const valid = /\S/.test(field);
+console.log(field); 
+
+
