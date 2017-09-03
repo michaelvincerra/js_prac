ea3be9641eb641837475d0456e88f2042f62a7e8
michaelvincerra <michael.vincerra@gmail.com>
2017-09-03 17:58:19 -0400

    Adds ch18, querying DOM elements.

--- ch18/js_browser.js
+++ ch18/js_browser.js
@@ -6,3 +6,19 @@ for(let i=0; i<node.childNodes.length; i++) {
         }
     }
     printDOM(document, '');
+
+
+// nodeType is an integer identifying what type of node it is.
+// Node.ELEMENT_NODE
+// Node.TEXT_NODE
+
+// document.getElementById -- every element tag can be assigned a unique ID (id="")
+
+// document.getElementsByClassName -- returns a collection of elements that have a given class name.
+
+// document.getElementsByTagName -- returns a collection of items that have a given tag name.
+
+// Note: You can convert an HTMLCollection to an array by using the spread operator: [...document.getElementsByTag Name(p)].
+
+//querySelector
+//querySelectorAll  ... allows to use CSS selectors. 
